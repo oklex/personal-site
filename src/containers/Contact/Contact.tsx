91c20fb4e5431c416b317ab025e9174a45b05d94
oklex <alexkim.347@gmail.com>
2019-08-18 23:01:20 -0700

    removed phone number

--- src/containers/Contact/Contact.tsx
+++ src/containers/Contact/Contact.tsx
@@ -11,10 +11,6 @@ class Contact extends React.Component<{}, {}> {
             <a href="mailto:alexkim.347@gmail.com"> alexkim.347@gmail.com</a>
           </p>
           <p>
-            Call me:
-            <a href="tel:7789858031"> 778.985.9031</a>
-          </p>
-          <p>
             Connect with me elsewhere:
             <a href="https://www.linkedin.com/in/akim-97/"> LinkedIn </a>-
             <a href="https://github.com/oklex"> Github</a>
